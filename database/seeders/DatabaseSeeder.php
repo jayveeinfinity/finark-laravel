119b9d1569ba268d59682c11651e5d51838030c1
unknown <jayveeinfinity@gmail.com>
2023-09-12 08:58:12 +0800

    Fix again seeder class

--- database/seeders/DatabaseSeeder.php
+++ database/seeders/DatabaseSeeder.php
@@ -15,8 +15,8 @@ class DatabaseSeeder extends Seeder
     {
         
         $this->call(PartnersSeeder::class);
-        $this->call(PartnerGroupsTableSeeder::class);
-        $this->call(PartnerGroupReferencesTableSeeder::class);
+        $this->call(PartnerGroupsSeeder::class);
+        $this->call(PartnerGroupReferencesSeeder::class);
         $this->call(RoleSeeder::class);
         $this->call(ServicePlansSeeder::class);
         $this->call(UsersTableSeeder::class);
